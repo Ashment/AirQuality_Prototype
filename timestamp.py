1e13b683c2c0209fdcd3d642e1b09e9d7d256e87
phuhung27 <26794032+phuhung27@users.noreply.github.com>
2021-04-17 17:19:01 -0400

    Added timestamp function.

---
+++ timestamp.py
@@ -0,0 +1,19 @@
+import time
+
+
+def add_timestamp(input_str):
+    current_time = time.ctime()
+
+    sep_time = time.strptime(current_time)
+
+    timestamp = time.asctime(sep_time)
+
+    return "[" + timestamp + "] " + input_str
+
+
+def main():
+    print(add_timestamp("sfw"))
+
+
+if __name__ == '__main__':
+    main()
